832e3d38fb189775366c796ed49a3516203dc510
comfort99 <shingangecomfort@gmail.com>
2024-03-11 01:09:19 +0200

    indentaton

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            #storage.new(self)
+            # storage.new(self)
 
     def __str__(self):
         """ Returns a string representation of base model """
--- models/user.py
+++ models/user.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
-class User(BaseModel):
-	email = ""
-	password = ""
-	first_name = ""
-	last_name = ""
 
+
+class User(BaseModel):
+    """User base class
+    containing right attributes"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
