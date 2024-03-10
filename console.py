5aafb3e5587f4706508cbff9a0a6391e8f5f202b
MercyAkpan <akpanmeme@gmail.com>
2024-03-10 15:19:12 +0100

    alx

--- console.py
+++ console.py
@@ -5,9 +5,14 @@ import sys
 from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
+	""" CLASS"""
 	prompt = "(hbnb) "
 	
 	def do_EOF(self, line):
