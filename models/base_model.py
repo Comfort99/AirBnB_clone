7e32d5fd5015641812ae2d531455561b9c9fbd75
comfort99 <shingangecomfort@gmail.com>
2024-03-10 16:05:08 +0200

    modified

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            #storage.new(self)
 
     def __str__(self):
         """ Returns a string representation of base model """
@@ -38,6 +38,7 @@ class BaseModel:
         """ update the current datetime """
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
